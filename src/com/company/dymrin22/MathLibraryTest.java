b6f3d791f08473e36340b9fcbd85867a0deb6a0c
Vadim Dobrin <zuka537122@gmail.com>
2022-01-27 01:45:24 +0200

    Fixed

--- src/com/company/dymrin22/MathLibraryTest.java
+++ src/com/company/dymrin22/MathLibraryTest.java
@@ -1,38 +1,78 @@
 package com.company.dymrin22;
 
 
-
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import java.util.stream.Stream;
 
 public class MathLibraryTest {
 
+    @Test
+    public void testThrowRuntimeException_whenNo4DigitFound_inPassedArray() {
+        Assertions.assertThrows(RuntimeException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                MathLibrary.getValuesAfterFourFromArray(new int[]{1, 2, 3, 5});
+            }
+        });
+    }
 
     @ParameterizedTest
-    @MethodSource("arraysToGetValue")
-    public void testShowNumbersAfterFour_inArray(int[] array) {
-        new MathLibrary().getValuesFromArray(array);
-
+    @NullAndEmptySource
+    void testThrowRuntimeException_whenNoDigitsFound_inPassedArray(int[] array) {
+        Assertions.assertThrows(RuntimeException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                MathLibrary.getValuesAfterFourFromArray(array);
+            }
+        });
     }
 
     @ParameterizedTest
     @MethodSource("arraysToGetValue")
-    public void testCheckArrayForNumbers(int[] array) {
-        if (!new MathLibrary().arrayChecking(array)) {
-            throw new IllegalArgumentException();
-        }
+    public void testShowNumbersAfterFour_inArray(int[] array, int[] expected) {
+        Assertions.assertArrayEquals(expected, MathLibrary.getValuesAfterFourFromArray(array));
 
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void testThrowRuntimeException_whenNoValuesFound_inPassedArray(int[] array) {
+        Assertions.assertThrows(RuntimeException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                MathLibrary.checkArrayForOneAndFour(array);
+            }
+        });
+    }
+
+    @ParameterizedTest
+    @MethodSource("arrayWithOneOrFourDigitProvider")
+    public void testCheckOneOrFourValuePresents_inPassedArray(int[] array) {
+        Assertions.assertTrue(MathLibrary.checkArrayForOneAndFour(array));
     }
 
     private static Stream<Arguments> arraysToGetValue() {
         return Stream.of(
-                Arguments.of(new int[]{3, 3, 4, 5, 9, 2, 4, 9, 0, 5}),
-                Arguments.of(new int[]{1, 5, 3, 0, 8, 1, 4, 0, 2, 6}),
-                Arguments.of(new int[]{2, 6, 9, 1, 0, 5, 7, 2, 3, 8}),
-                Arguments.of(new int[]{0, 2, 1, 3, 4, 1, 2, 3, 4, 6})
+                Arguments.of(new int[]{1, 2, 3, 4}, new int[0]),
+                Arguments.of(new int[]{1, 2, 3, 4, 5, 6}, new int[]{5, 6}),
+                Arguments.of(new int[]{1, 2, 3, 4, 5, 6, 4, 8, 9}, new int[]{8, 9})
+        );
+    }
+
+    private static Stream<int[]> arrayWithOneOrFourDigitProvider() {
+        return Stream.of(
+                new int[]{1},
+                new int[]{4},
+                new int[]{1, 2, 3},
+                new int[]{4, 3, 5, 6},
+                new int[]{1, 3, 5, 4}
         );
     }
 
